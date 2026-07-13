bcf3a6e73d9651480a9790a5e9c997f13a8e07d3
Zdenek Zambersky <zzambers@redhat.com>
2026-07-13 15:40:27 +0200

    BuildXmlHandler: simplification and improvements

--- src/main/java/resultsview/xml/BuildXmlHandler.java
+++ src/main/java/resultsview/xml/BuildXmlHandler.java
@@ -45,7 +45,6 @@ public class BuildXmlHandler extends SAXTreeHandler {
     private void init() {
         SAXTreeHandlerNode rootNode = new SAXTreeHandlerNode(null);
         SAXTreeHandlerNode buildNode = new SAXTreeHandlerNode("build");
-        SAXTreeHandlerNode matrixBuildNode = new SAXTreeHandlerNode("matrix-build");
         SAXTreeHandlerNode actionsNode = new SAXTreeHandlerNode("actions");
         SAXTreeHandlerNode kojiNode = new SAXTreeHandlerNode("hudson.plugins.scm.koji.KojiRevisionState");
         SAXTreeHandlerNode buildNode2 = new SAXTreeHandlerNode("build");
@@ -115,7 +114,8 @@ public class BuildXmlHandler extends SAXTreeHandler {
 
         };
         rootNode.addChild(buildNode.getName(), buildNode);
-        rootNode.addChild(matrixBuildNode.getName(), matrixBuildNode);
+        rootNode.addChild("matrix-build", buildNode);
+        rootNode.addChild("flow-build", buildNode);
         buildNode.addChild(actionsNode.getName(), actionsNode);
         actionsNode.addChild(kojiNode.getName(), kojiNode);
         kojiNode.addChild(buildNode2.getName(), buildNode2);
@@ -128,12 +128,6 @@ public class BuildXmlHandler extends SAXTreeHandler {
         buildNode.addChild(startTimeNode);
         buildNode.addChild(durationNode);
         buildNode.addChild(buildOnNode);
-
-        matrixBuildNode.addChild(resultNode);
-        matrixBuildNode.addChild(timestampNode);
-        matrixBuildNode.addChild(startTimeNode);
-        matrixBuildNode.addChild(durationNode);
-        matrixBuildNode.addChild(buildOnNode);
 
         setRootNode(rootNode);
     }
